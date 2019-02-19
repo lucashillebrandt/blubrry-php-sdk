313e0539988390b85c85bd122fd33e0a9a37cbe8
Lucas Hillebrandt <hillebrandt.lucas@gmail.com>
2019-02-19 07:39:06 -0800

    Initial implementation

--- src/Blubrry/REST/Resource/PodcastStatistics.php
+++ src/Blubrry/REST/Resource/PodcastStatistics.php
@@ -3,31 +3,59 @@
 namespace Blubrry\REST\Resource;
 
 /**
- * PodcastStatistics class 
+ * PodcastStatistics class
  *
  * @package Blubrry\REST\Resource
  */
 class PodcastStatistics {
+    /**
+     * Get Summary.
+     *
+     * @since 1.0.0
+     *
+     * @param string $program_keyword
+     * @param string $month
+     * @param string $year
+     */
     public function summary($program_keyword, $month = null, $year = null) {
         $path="/2/stats/$program_keyword/summary.json";
         $qs="";
 
-        if ( ! is_null( $month ) ) {
+        if (! is_null($month)) {
             $qs+="?month=$month";
         }
 
-        if ( ! is_null ( $year ) ) {
+        if (! is_null($year)) {
             $qs+="?year=$year";
         }
 
-        if ( ! is_null ( $year ) && ! is_null ( $month ) ) {
+        if (! is_null($year) && ! is_null($month)) {
             $qs="?month=${month}&year=$year";
         }
 
-        return \Blubrry\REST\API::request( $path . $qs, 'GET' );
+        return \Blubrry\REST\API::request($path . $qs, 'GET');
     }
     
-    public function totals() {
-        return 1;
+    /**
+     * Get totals from a specific podcast.
+     *
+     * @since 1.0.0
+     *
+     * @param string $program_keyword
+     * @param array $params
+     */
+    public function totals($program_keyword, $params) {
+        $path = "/2/stats/$program_keyword/totals.json";
+        $qs="?";
+
+        if (empty($params)) {
+            return false;
+        }
+    
+        foreach ($params as $key => $item) {
+            $qs+="${key}=${item}&";
+        }
+
+        return \Blubrry\REST\API::request($path . $qs, 'GET');
     }
 }
