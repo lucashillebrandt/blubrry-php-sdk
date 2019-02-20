dd161b518fe423ea815d220d3ff3dc272192704a
Lucas Hillebrandt <hillebrandt.lucas@gmail.com>
2019-02-20 12:21:31 -0800

    Implements API.php

--- src/Blubrry/REST/API.php
+++ src/Blubrry/REST/API.php
@@ -15,6 +15,39 @@ class API {
     const BLUBRRY_API_URL = 'https://api.blubrry.com';
 
     /**
+     * Media Hosting resource.
+     *
+     * @since 1.0.0
+     *
+     * @return \Blubrry\REST\Resource\MediaHosting Resource instance.
+     */
+    public function mediaHosting() {
+        return new \Blubrry\REST\Resource\MediaHosting();
+    }
+
+    /**
+     * Podcast Statistics resource.
+     *
+     * @since 1.0.0
+     *
+     * @return \Blubrry\REST\Resource\PodcastStatistics Resource instance.
+     */
+    public function podcastStatistics() {
+        return new \Blubrry\REST\Resource\PodcastStatistics();
+    }
+
+    /**
+     * Social resource.
+     *
+     * @since 1.0.0
+     *
+     * @return \Blubrry\REST\Resource\Social Resource instance.
+     */
+    public function social() {
+        return new \Blubrry\REST\Resource\Social();
+    }
+    
+    /**
      * Performs a request to the Blubrry REST API.
      *
      * @since 1.0.0
